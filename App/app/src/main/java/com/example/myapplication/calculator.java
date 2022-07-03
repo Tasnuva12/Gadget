257201dddc5bb2dc8e62ed7b2379c85ca73fd443
Tasnuva Mehnaz <tasnuva.mehnaz.98bd@gmail.com>
2022-07-03 21:34:57 +0600

    #calculator

--- App/app/src/main/java/com/example/myapplication/calculator.java
+++ App/app/src/main/java/com/example/myapplication/calculator.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
@@ -21,114 +25,28 @@ View view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //display
-        previousCalculation=view.findViewById(R.id.previousCalculationView);
-        display=view.findViewById(R.id.displayEditText);
-
-
-
-
 
     }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_calculator, container, false);
-
-
-
-
-
-
-        return view;
+        //display
+        previousCalculation=view.findViewById(R.id.previousCalculationView);
+        display=view.findViewById(R.id.displayEditText);
+      return view;
     }
-
     private void updateText(String strToAdd) {
         display.setText(strToAdd);
     }
-    public void zeroBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._0));
-    }
-
-    public void oneBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._1));
-    }
-
-    public void twoBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._2));
-    }
-
-    public void threeBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._3));
-    }
-
-    public void fourBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._4));
-    }
-
-    public void fiveBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._5));
-    }
-
-    public void sixBTNPush(View view) {
-
-        updateText(getView().getResources().getString(R.string._6));
-    }
-
-    public void sevenBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._7));
-    }
-
-    public void eightBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._8));
-    }
-
-    public void nineBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string._9));
-    }
-
-    public void dotBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.dot));
-    }
 
 
 
-    public void plusBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.plus));
-    }
-
-    public void minusBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.minus));
-    }
-
-    public void multiplyBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.x));
-    }
-
-    public void divisionBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.division));
-    }
-
-    public void firstBracketBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.start_bracket));
-    }
-
-    public void secondBracketBTNPush(View view) {
-        updateText(getView().getResources().getString(R.string.closing_bracket));
-    }
 
 
-
-    public void clearBTNPush(View view) {
-
-    }
-    public void backspaceBTNPush(View view) {
-
-    }
-    public void equalBTNPush(View view) {
-
-    }
 
 
 
